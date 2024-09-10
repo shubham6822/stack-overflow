6a486629a8657d1347afe7c0cba284b746a723eb
shubham6822 <sharmashubham6822@gmail.com>
2024-09-10 12:26:17 +0530

    WIP in ask-question page

--- src/app/(root)/(home)/ask-question/page.tsx
+++ src/app/(root)/(home)/ask-question/page.tsx
@@ -1,6 +1,12 @@
+import Question from "@/components/forms/Question";
 
-export default function AskQuestion() {
+export default function Page() {
     return (
-        <div>AskQuestion</div>
+        <div>
+            <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+            <div className="mt-9">
+                <Question />
+            </div>
+        </div>
     )
 }
---
+++ src/components/forms/Question.tsx
@@ -0,0 +1,6 @@
+"use client";
+export default function Question() {
+    return (
+        <div>Question</div>
+    )
+}
