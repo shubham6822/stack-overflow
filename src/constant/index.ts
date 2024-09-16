7215b3c1d9b7ee6e45a833fd5652692453ffb4ab
shubham6822 <sharmashubham6822@gmail.com>
2024-09-16 10:47:36 +0530

    fix jobs url

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -24,7 +24,7 @@ export const sidebarLinks: SidebarLink[] = [
     },
     {
         imgURL: "/assets/icons/suitcase.svg",
-        route: "/",
+        route: "/jobs",
         label: "Find Jobs",
     },
     {
